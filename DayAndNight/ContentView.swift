a36292784da11e7db7217266d3b735cdbf0a89ee
vsvami <106614918+vsvami@users.noreply.github.com>
2024-05-07 21:07:45 +0400

    Initial Commit

---
+++ DayAndNight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayAndNight
+//
+//  Created by Vladimir Dmitriev on 07.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayAndNight/DayAndNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayAndNightApp.swift
+//  DayAndNight
+//
+//  Created by Vladimir Dmitriev on 07.05.24.
+//
+
+import SwiftUI
+
+@main
+struct DayAndNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
